d41be5d5c078d96876ea55831a36311a1040942b
Pereira <gustavo.pereira@agilecontent.com>
2023-12-12 20:41:14 -0300

    Decompondo statement - usando extract function

--- Example1/example1.js
+++ Example1/example1.js
@@ -9,26 +9,8 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.PlayID];
-        let thisAmount = 0;
 
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 30) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play)
 
         volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -42,4 +24,29 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
 
     return result;
+}
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 30) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return thisAmount;
 }
