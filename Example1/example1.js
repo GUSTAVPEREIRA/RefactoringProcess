8b8c18f35aa8b79188c0f7d3df7895f916340991
Pereira <gustavo.pereira@agilecontent.com>
2023-12-12 22:48:46 -0300

    Applied Split loop in volumeCredits and applied replace temp with query and extract function

--- Example1/example1.js
+++ Example1/example1.js
@@ -1,22 +1,35 @@
 let example1 = {};
 example1.plays = "";
+example1.invoice = "";
 
 function statement(invoice, plays) {
     example1.plays = plays;
+    example1.invoice = invoice;
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
         result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+
+    volumeCredits += totalVolumeCredits();
 
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
 
     return result;
+}
+
+function totalVolumeCredits() {
+    let volumeCredits = 0;
+    
+    for (let perf of example1.invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+    
+    return volumeCredits;
 }
 
 function usd(aNumber) {
