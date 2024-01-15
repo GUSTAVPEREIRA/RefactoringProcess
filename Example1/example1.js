170c687c36d78558f2da0d14f34c866106ce50c2
pereira <gustavo.pereira@agilecontent.com>
2024-01-14 23:09:34 -0300

    Move playfor to data structure

--- Example1/example1.js
+++ Example1/example1.js
@@ -7,21 +7,28 @@ const data = {
 function statement(invoice, plays) {
     
     data.customer = invoice.customer,
-    data.performances = invoice.performances
-    data.plays = plays
+    data.plays = plays;
+    data.performances = invoice.performances.map(enrichPerformance);
 
     return renderPlainText(data);
+}
+
+function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance)
+    result.play = playFor(result);
+
+    return result;
 }
 
 function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
 
     for (let perf of data.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)`;
     }
 
     result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits`;
 
     return result;
 }
@@ -56,7 +63,7 @@ function volumeCreditsFor(perf) {
     let result = 0;
     result += Math.max(perf.audience - 30, 0);
 
-    if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+    if ("comedy" === perf.play.type) result += Math.floor(perf.audience / 5);
 
     return result;
 }
@@ -69,7 +76,7 @@ function playFor(aPerformance) {
 function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
         case "tragedy":
             result = 40000;
             if (aPerformance.audience > 30) {
@@ -85,7 +92,7 @@ function amountFor(aPerformance) {
             result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+            throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
 
     return result;
